fixed metrics computation

src/zone_detect/test/metrics.py
@@ -1,7 +1,6 @@
 import datetime
 import json
 from pathlib import Path
-from PIL import Image
 from matplotlib import pyplot as plt
 
 import numpy as np
@@ -9,6 +8,7 @@ import pandas as pd
 import rasterio
 from rasterio.windows import Window
 from sklearn.metrics import confusion_matrix
+from tqdm import tqdm
 
 from src.zone_detect.test.tiles import get_stride
 from src.zone_detect.test.pixel_operation import slice_pixels
@@ -46,26 +46,27 @@ def collect_paths_truth(config: dict, gt_dir: str) -> pd.DataFrame:
     gt_folder = Path(gt_dir)
 
     # get predictions
-    pred_folder = Path(config["output_path"].split("/")[:-2])
-    # after inference, output path is pred_folder/zone_name/timestamp
+    pred_folder = Path(config["output_path"])
+    timed_folders = [p for p in pred_folder.iterdir() if p.is_dir()]
 
     # dataframe with pred path, gt path and method single string
-    for zone in sorted(pred_folder.iterdir()):
-        # find an input file image
-        if not zone.is_dir():
-            continue
-        zone_name = zone.name
+    for timestamp in sorted(timed_folders):
 
-        zone_path = pred_folder / zone_name
-        pred_files = list(zone_path.rglob("*.tif"))
+        pred_files = list(timestamp.rglob("*.tif"))
+
+        # extract region info
+        region_id = str(pred_files[0].name).split("_IRC-ARGMAX-S_")[0]
+        dpt, zone_name = region_id.split("_")[:2], region_id.split("_")[2:]
+        zone_name = "_".join(zone_name)
 
         # corresponding ground truth
+        # we consider gt_folder the dpt folder
         gt_subfolder = gt_folder / zone_name
         gt_path = next(gt_subfolder.glob("*.tif"), None)
 
         for pred_path in pred_files:
-            method_id = Path(pred_path.name.split("ARGMAX-IRC-S_")[1]).stem
-            # zone_name_ARGMAX-IRC-S_size=128_stride=96_margin=32_padding=some-padding_stitching=exact_clipping
+            method_id = Path(pred_path.name.split("IRC-ARGMAX-S_")[1]).stem
+            # zone_name_IRC-ARGMAX-S_size=128_stride=96_margin=32_padding=some-padding_stitching=exact_clipping
             path_collection.append(
                 {
                     "pred_path": str(pred_path),
@@ -186,7 +187,8 @@ def batch_metrics(config: dict, gt_dir: str) -> list:
     grouped = df.groupby("method")
 
     # metrics for each method
-    for method, group in grouped:
+    # TODO : add a progress bar
+    for method, group in tqdm(grouped, desc="Computing metrics"):
 
         pred_paths = group["pred_path"].tolist()
         gt_paths = group["gt_path"].tolist()
@@ -194,10 +196,12 @@ def batch_metrics(config: dict, gt_dir: str) -> list:
         patch_confusion_matrices = []
         for pred_path, gt_path in zip(pred_paths, gt_paths):
             try:
-                target = (
-                    np.array(Image.open(gt_path)) - 1
-                )  # depending on classes starting at 1 or 0
-                preds = np.array(Image.open(pred_path))
+                # loading
+                with rasterio.open(pred_path) as src:
+                    preds = src.read(1)
+                with rasterio.open(gt_path) as src:
+                    target = src.read(1) - 1
+
                 patch_confusion_matrices.append(
                     confusion_matrix(
                         target.flatten(),
@@ -213,9 +217,11 @@ def batch_metrics(config: dict, gt_dir: str) -> list:
         confmat_cleaned = clean_confmat(sum_confmat, config)
 
         # metrics
-        per_c_ious, avg_ious = class_IoU(confmat_cleaned)
-        ovr_acc = overall_accuracy(confmat_cleaned)
-        per_c_fscore, avg_fscore = class_fscore(confmat_cleaned)
+        with np.errstate(divide="ignore", invalid="ignore"):
+            # nans are handled dont worry
+            per_c_ious, avg_ious = class_IoU(confmat_cleaned)
+            ovr_acc = overall_accuracy(confmat_cleaned)
+            per_c_fscore, avg_fscore = class_fscore(confmat_cleaned)
 
         # method parameters
         tile_size, stride, margin, padding, stitching = str(method).split("_")
@@ -316,7 +322,7 @@ def error_rate_patch(config: dict, out_dir: str, pred_filename: str = "") -> Non
 
     # useful info : method used, error rate
 
-    method = str(pred_path).split("ARGMAX-IRC-S_")[-1].split(".tif")[0]
+    method = str(pred_path).split("IRC-ARGMAX-S_")[-1].split(".tif")[0]
 
     out_path = Path(out_dir)
     out_path.mkdir(parents=True, exist_ok=True)

src/zone_detect/test/test.py
@@ -1,13 +1,11 @@
-import datetime
+import json
 from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 import rasterio
 
-from src.zone_detect.test.metrics import valid_truth
-from src.zone_detect.test.tiles import get_stride
-from src.zone_detect.utils import read_config
-from src.zone_detect.test.pixel_operation import slice_pixels
+from src.zone_detect.test.metrics import batch_metrics
+from src.zone_detect.utils import read_config, setup_device, setup_out_path
 
 
 def geogr_patches(
@@ -123,75 +121,6 @@ def ground_truth_geoconv(
     return geo_patches
 
 
-def error_rate_patch(config: dict, out_dir: str, pred_filename: str = "") -> None:
-    # output file
-    assert out_dir is not None, "Please provide an output path for the metrics"
-
-    truth_path = valid_truth(config)
-
-    # prediction path
-    pred_folder = Path(config["output_path"])
-    pred_path = pred_folder / pred_filename
-    if pred_filename == "":
-        pred_path = next(pred_folder.glob("*.tif"), None)  # select the first one
-    if pred_path is None:
-        raise ValueError(f"No prediction file found in {pred_folder}")
-
-    # load images in arrays
-    # PIL struggles (multi band, 16 bit, float32)
-    with rasterio.open(truth_path) as src:
-        target = src.read(1) - 1  # -1 to match the prediction
-    with rasterio.open(pred_path) as src:
-        pred = src.read(1)
-
-    # slice into patches
-    img_size = target.shape[0], target.shape[1]
-    patches = slice_pixels(
-        img_size,
-        config["img_pixels_detection"],
-        config["margin"],
-        get_stride(config)[0],
-    )
-    effective_patch_size = config["img_pixels_detection"] - 2 * config["margin"]
-    out_array = np.zeros(
-        (effective_patch_size, effective_patch_size),
-    )
-    # iterate over the patches and access images using patches indices ?
-    for patch in patches:
-        bottom, top, left, right = patch
-
-        target_patch = target[bottom:top, left:right]
-        pred_patch = pred[bottom:top, left:right]
-
-        # compute the error rate
-        # for each pixel, increment if not equal
-        # hard error rate
-        # replace / evaluate soft confidence
-        out_array += np.where(target_patch != pred_patch, 1, 0)
-
-    out_array = out_array / len(patches)
-
-    # i'm afraid of the memory
-
-    # useful info : method used, error rate
-
-    method = str(pred_path).split("ARGMAX-IRC-S_")[-1].split(".tif")[0]
-
-    out_path = Path(out_dir)
-    out_path.mkdir(parents=True, exist_ok=True)
-    out_path = out_path / f"error_rate_{method}_{datetime.datetime.now()}.png"
-
-    # save the error rate as a png
-    plt.figure(figsize=(10, 10))
-    plt.axis("off")
-    plt.imshow(out_array, cmap="hot", interpolation="nearest", vmin=0.025, vmax=0.25)
-    plt.colorbar()
-    plt.title("Error Rate for method : \n" + method)
-    plt.savefig(str(out_path))
-    plt.close()
-    print(f"Error rate saved to {out_path}")
-
-
 # example
 
 if __name__ == "__main__":
@@ -219,7 +148,7 @@ if __name__ == "__main__":
     # read config
     config = read_config(config)
 
-    # prediction files
+    """# prediction files
     # get a folder inside output_path
     list_preds = []
     pred_folder = Path(config["output_path"])
@@ -232,13 +161,20 @@ if __name__ == "__main__":
     for pred in pred_path.iterdir():
         if not pred.is_file() or not pred.name.endswith(".tif"):
             continue
-        list_preds.append(pred)
+        list_preds.append(pred)"""
 
-    for pred in list_preds:
-        sub_file = str(pred).split("/")[-2] + "/" + pred.name
-        # error rate
-        error_rate_patch(
-            config,
-            config["output_path"],
-            sub_file,
-        )
+    device, use_gpu = setup_device(config)
+    config, compare = setup_out_path(config, False)
+    gt_dir = Path(config["truth_path"]).parent.parent
+    metrics_out = config["metrics_out"]
+
+    out = Path(metrics_out).with_suffix(".json")
+
+    metrics_file = batch_metrics(config, str(gt_dir))
+
+    # save the metrics to a json file
+    json.dump(
+        metrics_file,
+        open(out, "w"),
+    )
+    print(f"Metrics saved to {out}")