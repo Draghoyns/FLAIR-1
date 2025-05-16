fixing speed metrics computation

src/zone_detect/compare.py
@@ -24,13 +24,12 @@ def inference(
     config: dict,
     samples: dict[str, torch.Tensor],
 ) -> tuple[np.ndarray, np.ndarray]:
-    imgs = samples["image"]
-    imgs = imgs.to(device, non_blocking=(device.type == "cuda"))
+    imgs = samples["image"].to(device, non_blocking=(device.type == "cuda"))
     if use_gpu:
         torch.cuda.synchronize()
     with torch.no_grad():
         logits = model(imgs)
-        if config["model_framework"]["model_provider"] == "HuggingFace":
+        if config.get("model_framework", {}).get("model_provider") == "HuggingFace":
             logits = logits.logits
         logits.to(device)
     predictions = torch.softmax(logits, dim=1)
@@ -48,19 +47,19 @@ def stitching(
     out: DatasetWriter,
     stitch: str,
     stride: int,
-    resolution: tuple[float, float],
 ) -> tuple[np.ndarray, Window]:
-    """Output of this is ready to be written"""
+    """Outputs patch handled, ready to be written"""
 
-    margin: int = config["margin"]  # only for clipping
-    img_pixels_detection = config["img_pixels_detection"]
-    output_type: str = config["output_type"]  # we only handle argmax for now
+    margin = config["margin"]  # only for clipping
+    img_size = config["img_pixels_detection"]
+    output_type = config["output_type"]  # we only handle argmax for now
 
+    i = index[0]
     sliced_box = [
-        sliced_dataframe.at[index[0], "left"],
-        sliced_dataframe.at[index[0], "right"],
-        sliced_dataframe.at[index[0], "bottom"],
-        sliced_dataframe.at[index[0], "top"],
+        sliced_dataframe.at[i, "left"],
+        sliced_dataframe.at[i, "right"],
+        sliced_dataframe.at[i, "bottom"],
+        sliced_dataframe.at[i, "top"],
     ]  # geo
 
     # align to resolution
@@ -71,32 +70,32 @@ def stitching(
         # removing margins
         prediction = prediction[
             :,
-            0 + margin : img_pixels_detection - margin,
-            0 + margin : img_pixels_detection - margin,
+            0 + margin : img_size - margin,
+            0 + margin : img_size - margin,
         ]
         prediction = convert(prediction, output_type)
 
         # get the window
-        sliced_patch_bounds = create_polygon_from_bounds(
-            sliced_box[0], sliced_box[1], sliced_box[2], sliced_box[3]
-        )
+        sliced_patch_bounds = create_polygon_from_bounds(*sliced_box)
         window = geometry_window(out, [sliced_patch_bounds], pixel_precision=6)
         window = window.round_shape(op="ceil", pixel_precision=4)
+        return prediction, window
 
     else:
 
         # _________GETTING_WINDOW__________#
         # out of bounds handling and get the patch plus the margin
 
+        i = index[0]
         bigbox = [
-            sliced_dataframe.at[index[0], "left_o"],
-            sliced_dataframe.at[index[0], "right_o"],
-            sliced_dataframe.at[index[0], "bottom_o"],
-            sliced_dataframe.at[index[0], "top_o"],
+            sliced_dataframe.at[i, "left_o"],
+            sliced_dataframe.at[i, "right_o"],
+            sliced_dataframe.at[i, "bottom_o"],
+            sliced_dataframe.at[i, "top_o"],
         ]  # geo
         oob = np.array(out_of_bounds(bigbox, sliced_box)).astype(int)
-        oob[0] = oob[0] * -1
-        oob[2] = oob[2] * -1
+        oob[0] *= -1
+        oob[2] *= -1
 
         bounding_box = np.array(sliced_box) + oob * margin  # geo
 
@@ -111,7 +110,7 @@ def stitching(
         # help averaging
         size = out.profile["width"], out.profile["height"]
         # yes be careful, sliced_box should be in pixel coord for this function
-        overlapping = patch_overlap(size, img_pixels_detection, sliced_box, stride)
+        overlapping = patch_overlap(size, img_size, sliced_box, stride)
 
         # note : be really careful where you have geo coord and pixel coord
         # TODO : stay at pixel level the longest possible
@@ -119,19 +118,15 @@ def stitching(
         if stitch == "average":  # only for class_prob
 
             prediction = prediction / overlapping
-            prediction = prediction + possible_overlap
-
+            prediction += possible_overlap
             prediction = convert(prediction, output_type)
 
             pass
         elif stitch == "average_weights":
-            # get weights
-            weights = patch_weights(img_pixels_detection)
-            # get distance map
-            distance_map = total_weights(size, img_pixels_detection, sliced_box, stride)
+            weights = patch_weights(img_size, sigma=0.5, mode="exp")
+            distance_map = total_weights(size, img_size, sliced_box, stride)
             prediction = prediction * weights / distance_map
-            prediction = prediction + possible_overlap
-
+            prediction += possible_overlap
             prediction = convert(prediction, output_type)
 
         elif stitch == "max":

src/zone_detect/dataset.py
@@ -11,17 +11,20 @@ from rasterio.enums import Resampling
 def convert(img: np.ndarray, img_type: str) -> np.ndarray:
     """Convert the image and keep the best class
     or keep all probabilities in separate bands"""
+
     if img_type == "class_prob":
         if img.max() > 1:
             info = np.iinfo(img.dtype)  # get input datatype information
-            img = img.astype(np.float32) / info.max  # normalize [0,1]
-        img = np.iinfo(np.uint8).max * img  # scale by 255
-        return img.astype(np.uint8)
+            if info:
+                img = img.astype(np.float32) / info.max  # normalize [0,1]
+        img = (img * 255).astype(np.uint8)
+        return img
+
     elif img_type == "argmax":
-        img_arg = np.argmax(img, axis=0)
-        img_arg = np.expand_dims(img_arg, axis=0).astype(np.uint8)
-        img_max = np.max(img, axis=0)
-        img_max = np.expand_dims(img_max, axis=0).astype(np.float32)
+        img_arg = np.argmax(img, axis=0).astype(np.uint8)
+        img_arg = np.expand_dims(img_arg, axis=0)
+        img_max = np.max(img, axis=0).astype(np.float32)
+        img_max = np.expand_dims(img_max, axis=0)
         return np.concatenate(
             [img_arg, img_max], axis=0
         )  # not sure about compatibility
@@ -37,7 +40,7 @@ class Sliced_Dataset(Dataset):
         self,
         dataframe: gpd.GeoDataFrame,
         img_path: str,
-        resolution: tuple[float],
+        resolution: tuple[float, float],
         bands: list[int],
         patch_detection_size: int,
         norma_dict: list,
@@ -47,63 +50,61 @@ class Sliced_Dataset(Dataset):
         self.img_path = img_path
         self.resolution = resolution
         self.bands = bands
+        self.num_bands = len(bands)
         self.height, self.width = patch_detection_size, patch_detection_size
         self.norma_dict = norma_dict[0]
-
+        self.norm_type = self.norma_dict["norm_type"]
+        self.norm_means = self.norma_dict["norm_means"]
+        self.norm_stds = self.norma_dict["norm_stds"]
         self.big_image = rasterio.open(self.img_path)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.dataframe)
 
-    def close_raster(self):
+    def close_raster(self) -> None:
         if self.big_image and not self.big_image.closed:
             self.big_image.close()
 
-    def normalization(
-        self, in_img: np.ndarray, norm_type: str, means: list, stds: list
-    ):
-        if norm_type == "custom":
-            if len(means) != len(stds):
+    def normalization(self, in_img: np.ndarray) -> np.ndarray:
+        if not self.norm_type in [
+            "custom",
+            "scaling",
+        ]:
+            print(
+                "Invalid normalization type: should be custom or scaling. Going with scaling."
+            )
+
+        if self.norm_type == "custom":
+            if len(self.norm_means) != len(self.norm_stds):
                 print(
                     "If custom, provided normalization means and stds should be of the same length. Going with scaling."
                 )
-                in_img = img_as_float(in_img)
-            else:
-                in_img = in_img.astype(np.float64)
-                for i in range(in_img.shape[0]):
-                    in_img[i] -= means[i]
-                    in_img[i] /= stds[i]
-        elif norm_type == "scaling":
-            in_img = img_as_float(in_img)
-        else:
-            print(
-                "Normalization argument should be 'scaling' or 'custom'. Going with scaling."
-            )
-            in_img = img_as_float(in_img)
+                return img_as_float(in_img)
+            img = in_img.astype(np.float64)
+            for i in range(self.num_bands):
+                img[i] = (img[i] - self.norm_means[i]) / self.norm_stds[i]
+            return img
 
-        return in_img
+        return img_as_float(in_img)
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> dict[str, torch.Tensor]:
         try:
-            bounds = self.dataframe.at[index, "geometry"].bounds
+            bounds = self.dataframe.geometry.iat[index].bounds
             src = self.big_image
 
             window = rasterio.windows.from_bounds(
-                bounds[0], bounds[1], bounds[2], bounds[3], src.meta["transform"]
+                *bounds, transform=src.meta["transform"]
             )
             patch_img = src.read(
                 indexes=self.bands,
                 window=window,
-                out_shape=(len(self.bands), self.height, self.width),
+                out_shape=(self.num_bands, self.height, self.width),
                 resampling=Resampling.bilinear,
                 boundless=True,
             )
 
             patch_img = self.normalization(
                 patch_img,
-                self.norma_dict["norm_type"],
-                self.norma_dict["norm_means"],
-                self.norma_dict["norm_stds"],
             )
 
             return {
@@ -113,3 +114,9 @@ class Sliced_Dataset(Dataset):
 
         except rasterio._err.CPLE_BaseError as error:  # type: ignore
             print(f"CPLE error {error}")
+            return {
+                "image": torch.zeros(
+                    (self.num_bands, self.height, self.width), dtype=torch.float32
+                ),
+                "index": torch.tensor(index, dtype=torch.int32),
+            }

src/zone_detect/main.py
@@ -1,8 +1,9 @@
 import json
 from pathlib import Path
-import os, sys
+import sys
 import datetime
 import warnings
+import numpy as np
 import rasterio
 import argparse
 from tqdm import tqdm
@@ -68,26 +69,32 @@ def conf_log(
     img_size: list[int],
 ) -> None:
     # Determine model template info based on provider
-    provider = config["model_framework"]["model_provider"]
+    mf = config["model_framework"]
+    provider = mf["model_provider"]
     if provider == "HuggingFace":
-        model_template = (
-            f"{provider} - {config['model_framework']['HuggingFace']['org_model']}"
-        )
+        model_template = f"{provider} - {mf['HuggingFace']['org_model']}"
     elif provider == "SegmentationModelsPytorch":
-        model_template = f"{provider} - {config['model_framework']['SegmentationModelsPytorch']['encoder_decoder']}"
+        model_template = (
+            f"{provider} - {mf['SegmentationModelsPytorch']['encoder_decoder']}"
+        )
     else:
         model_template = provider  # fallback if unknown
 
     compare_handling = "strategies" in config
+    compare = config["compare"]
+    strategies = config["strategies"]
 
-    compare_param = f"""
+    if compare:
+        compare_param = f"""
     |- overlapping strategy: {"handled" if compare_handling  else "exact"}
-    |- tiling comparison: {"yes" if (compare_handling and config['strategies']['tiling']['enabled']) else "no"}
-    |- stitching comparison: {"no" if not compare_handling else config['strategies']['stitching']['method']}
-    |- padding: {"not handled" if not compare_handling else config["strategies"]['padding_overall']} \n """
+    |- tiling comparison: {"yes" if (compare_handling and strategies['tiling']['enabled']) else "no"}
+    |- stitching comparison: {"no" if not compare_handling else strategies['stitching']['method']}
+    |- padding: {"not handled" if not compare_handling else strategies['padding_overall']} \n """
+    else:
+        compare_param = ""
 
-    print("    [ ] no comparison" if not config["compare"] else "    [x] comparison")
-    print(
+    print("    [ ] no comparison" if not compare else "    [x] comparison")
+    log = [
         f"""
     |- output path: {config['output_path']}
     |- output raster name: {config['output_name']}
@@ -105,24 +112,31 @@ def conf_log(
     |- model template: {model_template}
     |- device: {"cuda" if config['use_gpu'] else "cpu"}
     |- batch size: {config['batch_size']}
-    {compare_param if config["compare"] else ""}
     """
-    )
+    ]
+    print("\n".join(log + [compare_param]))
 
 
 # __________Prepare objects___________#
 def prepare_tiles(
     config: dict,
     stride: int,
-) -> tuple[GeoDataFrame, dict, tuple]:
-    ## slicing extent for overlapping detection
+) -> tuple[GeoDataFrame, dict, tuple[float, float]]:
+    """Slicing extent for overlapping detection"""
+    input_path = Path(config["input_img_path"])
+    patch_size = config["img_pixels_detection"]
+    margin = config["margin"]
+    output_name = config["output_name"]
+    output_path = Path(config["local_out"])
+    write_df = config["write_dataframe"]
+
     sliced_dataframe, profile, resolution, img_size = slice_extent(
-        in_img=Path(config["input_img_path"]),
-        patch_size=config["img_pixels_detection"],
-        margin=config["margin"],
-        output_name=config["output_name"],
-        output_path=Path(config["local_out"]),
-        write_dataframe=config["write_dataframe"],
+        in_img=input_path,
+        patch_size=patch_size,
+        margin=margin,
+        output_name=output_name,
+        output_path=output_path,
+        write_dataframe=write_df,
         stride=stride,
     )
     ## log
@@ -134,7 +148,7 @@ def prepare_tiles(
 
 def prepare_data(
     config: dict, stride: int
-) -> tuple[Sliced_Dataset, DataLoader, GeoDataFrame, dict, tuple]:
+) -> tuple[Sliced_Dataset, DataLoader, GeoDataFrame, dict]:
 
     channels = config["channels"]
     norma_task = config["norma_task"]
@@ -164,7 +178,7 @@ def prepare_data(
         pin_memory=True,
     )
 
-    return dataset, data_loader, sliced_dataframe, profile, resolution
+    return dataset, data_loader, sliced_dataframe, profile
 
 
 def prepare_model(config: dict, device: torch.device) -> torch.nn.Module:
@@ -221,39 +235,58 @@ def run_from_config(config: dict) -> None:
     """Run the pipeline from a config file"""
     # setting up device and log
     device, use_gpu = setup_device(config)
-    config = setup_out_path(config)
 
     run_pipeline(config, device, use_gpu)
 
 
 def run_pipeline(config: dict, device: torch.device, use_gpu: bool) -> None:
+    """Works for a single input image"""
+
+    # set up common output path
+    config = setup_out_path(config)
 
     # extracting config parameters
     output_type = config["output_type"]
     n_classes = config["n_classes"]
+    compare = config["compare"]
+    local_out = Path(config["local_out"])
+    compute_metrics = config["metrics"]
 
     # log
-    log_filename = os.path.join(
-        config["local_out"],
+    log_filename = local_out / Path(
         f"{config['output_name']}_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.log",
     )
-    sys.stdout = Logger(filename=log_filename)
+    sys.stdout = Logger(filename=str(log_filename))
     sys.stderr = sys.stdout
     print(f"    [LOGGER] Writing logs to: {log_filename}")
 
+    # model
     model = prepare_model(config, device)
 
-    if config["compare"]:
+    # set truth path
 
-        method_times = {}
+    if compute_metrics:
+        full_truth_path = Path(config["truth_path"])
+        with rasterio.open(full_truth_path) as src:
+            truth_array = src.read(1) - 1  # to start from 0
 
         # common to a zone (= one tif image)
-        metrics_json = config["local_out"] + "/metrics_per-patch_final.json"
+        dpt, zone = Path(config["input_img_path"]).parts[-3:-1]
+        metrics_json = local_out / Path(f"metrics_per-patch_{dpt}_{zone}.json")
+    else:
+        truth_array = np.zeros((1, 1), dtype=np.uint8)
+        metrics_json = Path()
+
+    if compare:
+
+        method_times = {}
 
         print(f"""    [ ] starting comparison...\n""")
 
         settings = gen_param_combination(config)
         for combi in settings:
+
+            method_metrics = []
 
             img_pixels_detection = combi["img_pixels_detection"]
             margin = combi["margin"]
@@ -267,7 +300,7 @@ def run_pipeline(config: dict, device: torch.device, use_gpu: bool) -> None:
             # start timer
             start_time = datetime.datetime.now()
 
-            dataset, data_loader, sliced_dataframe, profile, resolution = prepare_data(
+            dataset, data_loader, sliced_dataframe, profile = prepare_data(
                 config, stride
             )
             # prepare output raster
@@ -298,7 +331,6 @@ def run_pipeline(config: dict, device: torch.device, use_gpu: bool) -> None:
                         out,
                         stitch,
                         stride,
-                        resolution,
                     )
                     # write
                     if output_type == "argmax":
@@ -310,8 +342,8 @@ def run_pipeline(config: dict, device: torch.device, use_gpu: bool) -> None:
                             window=window,
                         )
 
-                    if config["metrics"]:
-                        # compute metrics
+                    if compute_metrics:
+                        # compute metrics per patch
                         inference_time = datetime.datetime.now() - start_time
                         inference_time = inference_time.total_seconds()
                         if method not in method_times:
@@ -319,8 +351,14 @@ def run_pipeline(config: dict, device: torch.device, use_gpu: bool) -> None:
                         else:
                             method_times[method].append(inference_time)
 
-                        compute_metrics_patch(
-                            prediction, window, config, method, metrics_json
+                        method_metrics.append(
+                            compute_metrics_patch(
+                                prediction,
+                                truth_array,
+                                window,
+                                config,
+                                method,
+                            )
                         )
 
             out.close()
@@ -329,21 +367,22 @@ def run_pipeline(config: dict, device: torch.device, use_gpu: bool) -> None:
             print(
                 f"""    [X] done writing to {path_out.split('/')[-1]} raster file.\n"""
             )
-            print(
-                f"""    [X] done writing metrics to {metrics_json.split('/')[-1]} file.\n"""
-            )
 
-            if config["metrics"]:
+            if compute_metrics:
                 config["times"] = method_times
+                print(
+                    f"""    [X] done writing metrics to {metrics_json.name} file.\n"""
+                )
+
+                with open(metrics_json, "w") as f:
+                    json.dump(method_metrics, f, indent=2)
 
     else:
 
         # default configuration : exact clipping and default sized tiling
 
         stride = get_stride(config)[0]
-        dataset, data_loader, sliced_dataframe, profile, resolution = prepare_data(
-            config, stride
-        )
+        dataset, data_loader, sliced_dataframe, profile = prepare_data(config, stride)
 
         # prepare output raster
         out, path_out = prepare_output(config, profile)
@@ -371,7 +410,6 @@ def run_pipeline(config: dict, device: torch.device, use_gpu: bool) -> None:
                     out,
                     "exact-clipping",
                     stride,
-                    resolution,
                 )
                 # write
                 if output_type == "argmax":
@@ -393,47 +431,54 @@ def run_pipeline(config: dict, device: torch.device, use_gpu: bool) -> None:
     sys.stdout = sys.__stdout__
 
 
-def batch_metrics_pipeline(config: dict, gt_dpt: Path) -> None:
+def batch_metrics_pipeline(
+    config: dict, truth_dpt: Path, device: torch.device, use_gpu: bool
+) -> None:
     """
     Compute metrics for a batch of images.
     Args:
-        gt_dir (str): Path to the ground truth directory of the department.
+        gt_dpt (Path): Path to the ground truth directory of the department.
         config (dict): Configuration, in which the parameters for the inference are specified
     """
 
     out_json = Path(config["metrics_out"])
 
     # output file
-    assert out_json is not None, "Please provide an output path for the metrics"
+    assert out_json, "Please provide an output path for the metrics"
 
     # __________INFERENCE__________#
     inputs_dpt = Path(config["input_path"])
 
-    for full_zone in sorted(inputs_dpt.iterdir()):
+    for full_zone in sorted(p for p in inputs_dpt.iterdir() if p.is_dir()):
 
         # find an input file image
-        if not full_zone.is_dir():
-            continue
         irc_path = next(full_zone.glob("*IRC.tif"), None)
         if irc_path is None:
             continue
 
-        dpt, zone = str(irc_path).split("/")[-3:-1]
-        truth_path = os.path.join(gt_dpt, zone)
-        truth_path = next(Path(truth_path).glob("*.tif"), None)
+        dpt, zone = irc_path.parts[-3:-1]
+        truth_dir = truth_dpt / zone
+        truth_path = next(Path(truth_dir).glob("*.tif"), None)
+        if truth_path is None:
+            print(f"No ground truth found for zone: {zone}")
+            continue
 
-        config["input_img_path"] = irc_path
-        config["truth_path"] = truth_path
-        config["output_name"] = str(irc_path).split("/")[-1].split(".")[0] + "-ARGMAX-S"
+        config.update(
+            {
+                "input_img_path": str(irc_path),
+                "truth_path": str(truth_path),
+                "output_name": f"{irc_path.stem}-ARGMAX-S",
+            }
+        )
 
         # Inference and saving the predictions
-        run_from_config(config)
+        run_pipeline(config, device, use_gpu)
 
     # we have all the predictions in the output folder
 
     out = out_json.with_suffix(".json")
 
-    metrics_file = batch_metrics(config, gt_dpt)
+    metrics_file = batch_metrics(config, truth_dpt)
 
     # save the metrics to a json file
     json.dump(
@@ -453,10 +498,10 @@ def main():
     config, device, use_gpu = setup(args)
 
     if args.batch_mode:
-        gt_dir = config["truth_root"]
-        gt_dpt = gt_dir + "/" + config["truth_path"].split("/")[-3]
+        gt_dir = Path(config["truth_root"])
+        gt_dpt = gt_dir / config["truth_path"].parts[-3]
 
-        batch_metrics_pipeline(config, Path(gt_dpt))
+        batch_metrics_pipeline(config, gt_dpt, device, use_gpu)
     else:
         run_pipeline(config, device, use_gpu)
 

src/zone_detect/slicing_job.py
@@ -29,55 +29,49 @@ def slice_extent(
     with rasterio.open(in_img) as src:
         img_width, img_height = src.read(1).shape
         profile = src.profile
-        left_overall, bottom_overall, right_overall, top_overall = src.bounds
-        resolution = abs(round(src.res[0], 5)), abs(round(src.res[1], 5))
+        min_x, min_y, max_x, max_y = src.bounds
+        resolution_x, resolution_y = map(lambda r: abs(round(r, 5)), src.res)
 
     # geo conversion
-    geo_output_size = [patch_size * resolution[0], patch_size * resolution[1]]
-    geo_margin = [margin * resolution[0], margin * resolution[1]]
+    geo_output_w, geo_output_h = patch_size * resolution_x, patch_size * resolution_y
+    geo_margin_x, geo_margin_y = margin * resolution_x, margin * resolution_y
 
     if stride:
-        geo_step = [stride * resolution[0], stride * resolution[1]]
+        geo_step = [stride * resolution_x, stride * resolution_y]
     else:  # default
         geo_step = [
-            geo_output_size[0] - (2 * geo_margin[0]),
-            geo_output_size[1] - (2 * geo_margin[1]),
+            geo_output_w - (2 * geo_margin_x),
+            geo_output_h - (2 * geo_margin_y),
         ]
-
-    min_x, min_y = left_overall, bottom_overall
-    max_x, max_y = right_overall, top_overall
 
     # initializing
     tmp_list = []
     geo_patches = set()  # To track unique patches
 
-    X = np.arange(min_x - geo_margin[0], max_x + geo_margin[0], geo_step[0])
-    Y = np.arange(min_y - geo_margin[1], max_y + geo_margin[1], geo_step[1])
+    X = np.arange(min_x - geo_margin_x, max_x + geo_margin_x, geo_step[0])
+    Y = np.arange(min_y - geo_margin_y, max_y + geo_margin_y, geo_step[1])
 
     for x_coord in X:
-        for y_coord in Y:
-            # for each patch
 
-            # Adjust last column to ensure proper alignment
-            if x_coord + geo_output_size[0] > max_x + geo_margin[0]:
-                x_coord = max_x + geo_margin[0] - geo_output_size[0]
+        # Adjust last column to ensure proper alignment
+        if x_coord + geo_output_w > max_x + geo_margin_x:
+            x_coord = max_x + geo_margin_x - geo_output_w
+
+        for y_coord in Y:
             # Adjust last row
-            if y_coord + geo_output_size[1] > max_y + geo_margin[1]:
-                y_coord = max_y + geo_margin[1] - geo_output_size[1]
+            if y_coord + geo_output_h > max_y + geo_margin_y:
+                y_coord = max_y + geo_margin_y - geo_output_h
 
             # Define patch boundaries, geo, absolute position
-            left = x_coord + geo_margin[0]
-            right = x_coord + geo_output_size[0] - geo_margin[0]
-            bottom = y_coord + geo_margin[1]
-            top = y_coord + geo_output_size[1] - geo_margin[1]
-
             # Ensure patches don't go outside raster bounds
-            right = min(right, max_x)
-            top = min(top, max_y)
+            left = x_coord + geo_margin_x
+            right = min(x_coord + geo_output_w - geo_margin_x, max_x)
+            bottom = y_coord + geo_margin_y
+            top = min(y_coord + geo_output_h - geo_margin_y, max_y)
 
             col, row = (
-                int((y_coord - min_y) // resolution[0]) + 1,
-                int((x_coord - min_x) // resolution[1]) + 1,
+                int((y_coord - min_y) // resolution_x) + 1,
+                int((x_coord - min_x) // resolution_y) + 1,
             )
 
             # Unique identifier for patch
@@ -98,15 +92,15 @@ def slice_extent(
                     "bottom": bottom,
                     "right": right,
                     "top": top,
-                    "left_o": left_overall,
-                    "bottom_o": bottom_overall,
-                    "right_o": right_overall,
-                    "top_o": top_overall,
+                    "left_o": min_x,
+                    "bottom_o": min_y,
+                    "right_o": max_x,
+                    "top_o": max_y,
                     "geometry": create_box_from_bounds(
                         x_coord,
-                        x_coord + geo_output_size[0],
+                        x_coord + geo_output_w,
                         y_coord,
-                        y_coord + geo_output_size[1],
+                        y_coord + geo_output_h,
                     ),
                 }
                 tmp_list.append(row_d)
@@ -121,7 +115,7 @@ def slice_extent(
             driver="GPKG",
         )
 
-    return gdf_output, profile, resolution, [img_width, img_height]
+    return gdf_output, profile, (resolution_x, resolution_y), [img_width, img_height]
 
 
 def slice_extent_separate(

src/zone_detect/test/metrics.py
@@ -11,37 +11,51 @@ from sklearn.metrics import confusion_matrix
 from tqdm import tqdm
 
 from src.zone_detect.test.pixel_operation import slice_pixels
-from src.zone_detect.utils import get_truth_path, info_extract
+from src.zone_detect.utils import info_extract
 
 
 #### UTILS ####
 def clean_confmat(confmat: np.ndarray, config: dict) -> np.ndarray:
     #### CLEAN REGARDING WEIGHTS FOR METRICS CALC :
-    weights = np.array([config["classes"][i][0] for i in config["classes"]])
+    weights = np.array([class_info[0] for class_info in config["classes"].values()])
     unused_classes = np.where(weights == 0)[0]
-    confmat_cleaned = np.delete(confmat, unused_classes, axis=0)  # remove rows
-    confmat_cleaned = np.delete(
-        confmat_cleaned, unused_classes, axis=1
-    )  # remove columns
+    if unused_classes.size > 0:
+        confmat_cleaned = np.delete(confmat, unused_classes, axis=0)  # remove rows
+        confmat_cleaned = np.delete(
+            confmat_cleaned, unused_classes, axis=1
+        )  # remove columns
 
-    return confmat_cleaned
+        return confmat_cleaned
+    return confmat
 
 
 def valid_truth(config: dict) -> Path:
     """Check if the ground truth path is valid and coherent with the input path :
     the zone should be the same in both paths.
     """
-    truth_path = config["truth_path"]
+    truth_path = Path(config["truth_path"])
     # verify coherence with input path
-    sanity_check = config["input_img_path"].split("/")[-3:-1]  # zone
-    # sanity_check[0] = sanity_check[0][1:]
-    # because there's a D sometimes and sometimes not
-    truth_check = truth_path.split("/")[-3:-1]
+    sanity_check = str(config["input_img_path"]).split("/")[-3:-1]  # zone
+    truth_check = list(truth_path.parts[-3:-1])
     if truth_check != sanity_check:
         raise ValueError(
             f"Ground truth path {truth_path} does not match input path {config['input_img_path']}"
         )
     return Path(truth_path)
+
+
+def get_truth_path(pred_path: Path, truth_dir: Path) -> Path:
+    zone_name = info_extract(pred_path)["zone"]
+
+    # corresponding ground truth
+    # we consider gt_folder the dpt folder
+    truth_subdir = truth_dir / zone_name
+    truth_path = next(truth_subdir.glob("*.tif"), None)
+    if truth_path is None:
+        raise FileNotFoundError(
+            f"Ground truth file not found in {truth_subdir}. Please check the folder."
+        )
+    return truth_path
 
 
 def collect_paths_truth(config: dict, truth_dir: Path) -> pd.DataFrame:
@@ -59,7 +73,7 @@ def collect_paths_truth(config: dict, truth_dir: Path) -> pd.DataFrame:
 
         for pred_path in pred_files:
             method_id = Path(pred_path.name.split("IRC-ARGMAX-S_")[1]).stem
-            # zone_name_IRC-ARGMAX-S_size=128_stride=96_margin=32_padding=some-padding_stitching=exact-clipping
+            # dpt_zone-name_IRC-ARGMAX-S_size=128_stride=96_margin=32_padding=some-padding_stitching=exact-clipping
             path_collection.append(
                 {
                     "pred_path": str(pred_path),
@@ -108,8 +122,12 @@ def class_fscore(npcm):
 
 #### COMPUTATION ####
 def compute_metrics_patch(
-    pred_patch: np.ndarray, window: Window, config: dict, method: str, out_json: str
-) -> None:
+    pred_patch: np.ndarray,
+    truth: np.ndarray,
+    window: Window,
+    config: dict,
+    method: str,
+) -> dict:
     """
     Patch metrics can be computed before the stitching ,
     or once the whole image is built.
@@ -118,23 +136,26 @@ def compute_metrics_patch(
         pred_patch (np.ndarray): Predicted patch.
         window (Window): Window object for the patch.
         config (dict): Configuration, in which the parameters for the inference are specified
-        out_json (str): Path to the output JSON file for metrics. If the file exists, it will be overwritten.
+        out_json (Path): Path to the output JSON file for metrics. If the file exists, it will be overwritten.
             You better put in the name if it's raw (before stitching) or after.
     """
 
-    # get ground truth path from config
-    truth_path = valid_truth(config)
+    # raise error if invalid truth
+    valid_truth(config)
 
-    with rasterio.open(truth_path) as src:
-        # only supports argmax for now
-        target = src.read(1, window=window) - 1
+    target = truth[
+        window.row_off : window.row_off + window.height,
+        window.col_off : window.col_off + window.width,
+    ]
+
+    classes = config["classes"]
 
     #### compute metrics
     # confusion matrix
     confmat = confusion_matrix(
         target.flatten(),
         pred_patch[0].flatten(),
-        labels=list(range(int(len(config["classes"])))) + [255],
+        labels=list(range(int(len(classes)))) + [255],
     )
 
     confmat_cleaned = clean_confmat(confmat, config)
@@ -146,7 +167,6 @@ def compute_metrics_patch(
         per_c_fscore, avg_fscore = class_fscore(confmat_cleaned)
 
     # save metrics to a json file : raw or post-stitching
-    out = Path(out_json)
     key = f"{method}_{window.col_off}_{window.row_off}"
     metrics = {
         key: {
@@ -160,16 +180,12 @@ def compute_metrics_patch(
                 ovr_acc,
                 avg_fscore,
             ],
-            "classes": list(
-                np.array([config["classes"][i][1] for i in config["classes"]])
-            ),
+            "classes": [classes[i][1] for i in classes],
             "per_class_iou": list(per_c_ious),
             "per_class_fscore": list(per_c_fscore),
         }
     }
-    with open(out, "a") as f:
-        json.dump(metrics, f)
-        f.write("\n")
+    return metrics
 
 
 def batch_metrics(config: dict, truth_dir: Path) -> list:
@@ -259,9 +275,16 @@ def batch_metrics(config: dict, truth_dir: Path) -> list:
 
 
 def error_rate_loop(truth_dir: Path, out_dir: Path, pred_dir: Path) -> None:
+    """Args:
+    pred_dir (Path): the directory with predictions
+    out_dir (Path): the output directory for the error rate
+    truth_dir (Path): the ground truth directory of the department"""
     dic = {}
 
-    for pred_path in pred_dir.iterdir():
+    # get all tif files in the pred_dir
+    tif_files = list(pred_dir.rglob("*.tif"))
+
+    for pred_path in tqdm(tif_files, desc="Computing error rate"):
 
         # get corresponding truth file
         truth_file = get_truth_path(pred_path, truth_dir)
@@ -270,7 +293,7 @@ def error_rate_loop(truth_dir: Path, out_dir: Path, pred_dir: Path) -> None:
             truth_file=truth_file,
             out_dir=out_dir,
             pred_path=pred_path,
-            dic={},
+            dic=dic,
             save=False,
         )
 
@@ -286,10 +309,11 @@ def error_rate_loop(truth_dir: Path, out_dir: Path, pred_dir: Path) -> None:
         else:
             methods[method] += dic[key]
             total[method] += 1
+
     for key in methods.keys():
         methods[key] = methods[key] / total[key]
         # save the error rate as a png
-        autoscale = False
+        autoscale = True
         if autoscale:
             vmin = np.min(methods[key])
             vmax = np.max(methods[key])
@@ -305,6 +329,8 @@ def error_rate_loop(truth_dir: Path, out_dir: Path, pred_dir: Path) -> None:
         plt.title("Error Rate for method : \n" + key)
         plt.savefig(str(out_dir / f"error_rate_{key}.png"))
         plt.close()
+
+        print(f"Error rate saved to {out_dir / f'error_rate_{key}.png'}")
 
 
 # not incorporated in the pipeline, but maybe as option ?

src/zone_detect/utils.py
@@ -110,33 +110,32 @@ def gen_param_combination(config: dict) -> list:
     Handles single case or iterative case."""
     combi = []
 
-    # TODO
-    padding_list = config["strategies"]["padding_overall"]
-    if padding_list == []:
+    # TODO : add differet padding strategies
+    padding_list = config.get("strategies", {}).get("padding_overall", [])
+    if not padding_list:
         padding_list = ["no-padding"]
 
     # configuration for comparison
-
-    if config["strategies"]["tiling"]["enabled"]:
-        tile_size_list = config["strategies"]["tiling"]["size_range"]
+    tiling_cfg = config.get("strategies", {}).get("tiling", {})
+    if tiling_cfg.get("enabled", False):
+        tile_size_list = tiling_cfg.get("size_range", [config["img_pixels_detection"]])
     else:
         tile_size_list = [config["img_pixels_detection"]]
 
-    if config["strategies"]["stitching"]["enabled"]:
-        margin_list = config["strategies"]["stitching"]["margin"]
-        stitching_methods = config["strategies"]["stitching"]["methods"]
-    else:  # default stitching : exact clipping
+    stitching_cfg = config.get("strategies", {}).get("stitching", {})
+    # default stitching : exact clipping
+    if stitching_cfg.get("enabled", False):
+        margin_list = stitching_cfg.get("margin", [config["margin"]])
+        stitching_methods = stitching_cfg.get("methods", ["exact-clipping"])
+    else:
         margin_list = [config["margin"]]
         stitching_methods = ["exact-clipping"]
 
-    # basically a grid comparison
-    # could probably benefit from some optimization but ehhh
-
     for padding in padding_list:
         for img_pixels_detection in tile_size_list:
-            config["img_pixels_detection"] = img_pixels_detection
             for margin in margin_list:
-                config["margin"] = int(margin * img_pixels_detection)
+                if margin < 1:
+                    margin = int(margin * img_pixels_detection)
                 # skip if parameters are not valid
                 if img_pixels_detection <= 2 * margin:
                     print(
@@ -144,12 +143,15 @@ def gen_param_combination(config: dict) -> list:
                     )
                     continue
 
-                stride_list = get_stride(config)
+                # avoid mutation
+                tmp_config = config.copy()
+                tmp_config["margin"] = margin
+                tmp_config["img_pixels_detection"] = img_pixels_detection
+
+                stride_list = get_stride(tmp_config)
+
                 for stride in stride_list:
                     for stitch in stitching_methods:
-
-                        config["overlap_strat"] = stitch != "exact-clipping"
-                        config["strategies"]["stitching"]["method"] = stitch
 
                         param = {
                             "img_pixels_detection": img_pixels_detection,
@@ -181,9 +183,11 @@ def info_extract(file: Path) -> dict:
     # region info
     region = region.split("_")
     dpt, zone = region[:2], region[2:]
-    info["dpt"] = "D" + "_".join(dpt)
+    if not dpt[0].startswith("D"):
+        info["dpt"] = "D" + "_".join(dpt)
     info["zone"] = "_".join(zone)
     # method info
+    info["method"] = method
     method = method.split("_")
     for param in method:
         if param.startswith("size="):
@@ -203,33 +207,21 @@ def info_extract(file: Path) -> dict:
     return info
 
 
-def get_truth_path(pred_path: Path, truth_dir: Path) -> Path:
-    zone_name = info_extract(pred_path)["zone"]
-
-    # corresponding ground truth
-    # we consider gt_folder the dpt folder
-    truth_subdir = truth_dir / zone_name
-    truth_path = next(truth_subdir.glob("*.tif"), None)
-    if truth_path is None:
-        raise FileNotFoundError(
-            f"Ground truth file not found in {truth_subdir}. Please check the folder."
-        )
-    return truth_path
-
-
 #### SETUP ####
 def setup_out_path(config: dict) -> dict:
     """Setup the output directory"""
-    Path(config["output_path"]).mkdir(parents=True, exist_ok=True)
+    output = Path(config["output_path"])
+    output.mkdir(parents=True, exist_ok=True)
+    child_dir = output
 
     if config["compare"]:
         # create a directory with a unique id
         current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-        child_dir = os.path.join(config["output_path"], current_time)
+        child_dir = child_dir / Path(current_time)
         os.makedirs(child_dir, exist_ok=True)
-        config["local_out"] = child_dir
-    else:
-        config["local_out"] = config["output_path"]
+        print(f"Creating output directory: {child_dir}")
+
+    config["local_out"] = child_dir
 
     return config
 
@@ -244,10 +236,9 @@ def setup_device(config: dict) -> tuple[torch.device, bool]:
 
 
 def setup(args) -> tuple[dict, torch.device, bool]:
-    """Setup the device and output path"""
+    """Setup the device"""
     config = read_config(args)
     device, use_gpu = setup_device(config)
-    config = setup_out_path(config)
 
     return config, device, use_gpu
 
@@ -276,6 +267,11 @@ def setup_indiv_path(config: dict, identifier: str) -> tuple[dict, str]:
     except Exception as error:
         print(f"Something went wrong during detection configuration: {error}")
         raise error  # avoid silent failure
+
+
+def open_images():
+    """Get the input image array.
+    Optional : get the ground truth image array."""
 
 
 #### ROUNDING AND ALIGNING ####