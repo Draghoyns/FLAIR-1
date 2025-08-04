6cd9abeb3ab91ea53cf438070f3ccae3d0cc4bcb
Draghoyns <sakula.hys@polytechnique.org>
2025-08-04 16:14:15 +0200

    optimization options placeholder functions

---
+++ src/zone_detect/optimization/quantization/methods.py
@@ -0,0 +1,25 @@
+def with_quanto():
+    """
+    This function is a placeholder for the quanto quantization method.
+    It is not implemented yet and serves as a reminder to implement it in the future.
+    """
+    print("Quantization not implemented for this dtype, this is a placeholder.")
+    pass
+
+
+def with_torchao():
+    """
+    This function is a placeholder for the torchao quantization method.
+    It is not implemented yet and serves as a reminder to implement it in the future.
+    """
+    print("Quantization not implemented for this dtype, this is a placeholder.")
+    pass
+
+
+def with_torch():
+    """
+    This function is a placeholder for the torch quantization method.
+    It is not implemented yet and serves as a reminder to implement it in the future.
+    """
+    print("Quantization not implemented for this dtype, this is a placeholder.")
+    pass
